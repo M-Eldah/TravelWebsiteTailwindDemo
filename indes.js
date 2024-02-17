252116fef5fd36289eeef1892c700a608bd16cf0
M-Eldah <mohamedahmedeldah@gmail.com>
2024-02-17 21:58:21 +0200

    Adding check to prevent repeated change in location before previous change finished

--- indes.js
+++ indes.js
@@ -1,14 +1,18 @@
+chaning = false;
 function selectDestination(id, path, credit) {
-    console.log(id);
-    const element = document.getElementsByClassName('selectedDestination');
-    console.log(element);
-    if (element.length > 0)
-        element[0].classList.remove("selectedDestination")
-    document.getElementById(id).classList.add('selectedDestination');
-    document.getElementById("curtain").classList.add("bg-black");
-    setTimeout(() => {
-        document.getElementById("credit").innerText = credit;
-        document.getElementById("backgroundimg").src = path;
-        document.getElementById("curtain").classList.remove("bg-black");
-    }, 1400)
+    if (chaning === false) {
+        chaning = true;
+        const element = document.getElementsByClassName('selectedDestination');
+        if (element.length > 0)
+            element[0].classList.remove("selectedDestination")
+        document.getElementById(id).classList.add('selectedDestination');
+        document.getElementById("curtain").classList.add("bg-black");
+        setTimeout(() => {
+            document.getElementById("credit").innerText = credit;
+            document.getElementById("backgroundimg").src = path;
+            document.getElementById("curtain").classList.remove("bg-black");
+            chaning = false;
+        }, 1400)
+    }
+
 }
